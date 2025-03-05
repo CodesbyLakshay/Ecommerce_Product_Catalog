311062195c026f97a52a7d4994378b39538d9567
lakshayjain <lakshay2007jain@gmail.com>
2025-03-05 23:08:09 +0530

    Concurrent Modification

--- src/main/java/com/ecom/productcatalog/model/Product.java
+++ src/main/java/com/ecom/productcatalog/model/Product.java
@@ -1,6 +1,7 @@
 package com.ecom.productcatalog.model;
 
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,7 @@ public class Product {
 
     @ManyToOne
     @JoinColumn(name = "category_id" , nullable = false)
+    @JsonBackReference
     private Category category;
 
 }
