2c6e016ce60f45a84a39b5c5820df65b1c52bd3e
lakshayjain <lakshay2007jain@gmail.com>
2025-03-08 00:12:12 +0530

    fetch data back-to-front

--- ecom-catalog-react/src/App.jsx
+++ ecom-catalog-react/src/App.jsx
@@ -1,16 +1,26 @@
-
+import { useEffect,useState } from 'react';
+import ProductList from './ProductList';
 import './App.css'
 
 function App() {
 
- const[products , setProducts] = useState([]);
+const[products , setProducts] = useState([]);
 
+useEffect(() => {
+    fetch('http://localhost:8080/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data));
+    },[]);
   return (
-   <div>
-<button type="button" class="btn-close" aria-label="Close"></button>
-<button type="button" class="btn-close" aria-label="Close"></button>
-<button type="button" class="btn-close" aria-label="Close"></button>
-<button type="button" class="btn-close" aria-label="Close"></button>
+   <div className='container'>
+       <div>
+           {products.length ? (
+               //display products
+               <ProductList products={products} />
+               ) : (
+               <p>No products found</p>)}
+       </div>
+
    </div>
   )
 }
