6fcedc72d2567ee66aad317e69024468ea94b8fb
lakshayjain <lakshay2007jain@gmail.com>
2025-03-08 00:35:50 +0530

    Update

--- ecom-catalog-react/src/App.jsx
+++ ecom-catalog-react/src/App.jsx
@@ -5,14 +5,23 @@ import './App.css'
 function App() {
 
 const[products , setProducts] = useState([]);
+const[categories , setCategories] = useState([]);
+const[selectedCategory , setSelectedCategory] = useState(null);
+const[searchTerm , setSearchTerm] = useState("");
+const[sortOrder , setSortOrder] = useState("ascending");
 
 useEffect(() => {
     fetch('http://localhost:8080/api/products')
     .then(response => response.json())
     .then(data => setProducts(data));
+
+    fetch('http://localhost:8080/api/categories')
+        .then(response => response.json())
+        .then(data => setCategories(data));
     },[]);
   return (
    <div className='container'>
+       <h1 className='my-4'>Product Catalog</h1>
        <div>
            {products.length ? (
                //display products
